9d695170fd7a7b7f458c0ea710de2390d593f3c9
ctmai202122 <ctmai2021@gmail.com>
2023-05-23 15:06:52 +0200

    corrections fonctionnement methode MVC

--- App/Controllers/deleteContreeController.php
+++ App/Controllers/deleteContreeController.php
@@ -1,6 +1,9 @@
 <?php
 // Inclure le fichier du modèle "Contree"
 use Broceliande\Models\Contree;
+// Initialisation des variables de message
+$message = "";
+$erreur = "";
 
 // Vérifier si le formulaire a été soumis et si l'ID de la contrée est défini
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["idContree"])) {
@@ -13,18 +16,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["idContree"])) {
     // Supprimer la contrée en utilisant la méthode appropriée du modèle
     $contreeModel->delete($idContree);
 
+
     // Message de succès
     $message = "La contrée a été supprimée avec succès.";
-
-    // Redirection vers la page de gestion des contrées
-    echo '<script>alert("'.$message.'"); location.href = "?action=gestionContree";</script>';
-    exit();
 } else {
     // Message d'erreur
     $erreur = "Une erreur s'est produite lors de la suppression de la contrée.";
-
-    // Redirection vers la page de gestion des contrées
-    echo '<script>alert("'.$erreur.'"); location.href = "?action=gestionContree";</script>';
-    exit();
 }
+
+// Redirection vers la page de gestion des contrées
+header("Location: ?action=gestionContree&message=" . urlencode($message) . "&erreur=" . urlencode($erreur));
+exit();
 ?>
