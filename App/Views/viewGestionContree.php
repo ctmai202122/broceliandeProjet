32888570e52b42ab64ea3a7c9b99f0a70aff7a4d
ctmai202122 <ctmai2021@gmail.com>
2023-05-24 12:59:11 +0200

    Modification buttons add delete

--- App/Views/viewGestionContree.php
+++ App/Views/viewGestionContree.php
@@ -1,8 +1,8 @@
 <?php
 // Inclusion du fichier de vue pour l'en-tête
 include_once(__DIR__ . '/viewHeader.php');
-    //Inclure la vue de la nav admin
-    include_once(__DIR__ . '/viewMenuAdmin.php');
+//Inclure la vue de la nav admin
+include_once(__DIR__ . '/viewMenuAdmin.php');
 
 ?>
 <div class="containerGestion">
@@ -15,11 +15,11 @@ include_once(__DIR__ . '/viewHeader.php');
         <form method="post" action="?action=addContree" enctype="multipart/form-data">
             <div class="form-group mt-4">
                 <label for="titre">Titre : <span class="required">*</span></label>
-                <input type="text" id="titre" name="titre" placeholder="Titre de la contrée..."  class="form-control" required>
+                <input type="text" id="titre" name="titre" placeholder="Titre de la contrée..." class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="contenu">Contenu : <span class="required">*</span></label>
-                <textarea class="col-12"id="contenu" name="contenu" placeholder="Description de la contrée..." rows="9"class="form-control" required></textarea>
+                <textarea class="col-12" id="contenu" name="contenu" placeholder="Description de la contrée..." rows="9" class="form-control" required></textarea>
             </div>
             <div class="form-group">
                 <label for="photo">Photo : </label>
@@ -36,46 +36,45 @@ include_once(__DIR__ . '/viewHeader.php');
             </div>
             <div class="form-group">
                 <label for="commune">Commune : <span class="required">*</span></label>
-                <input type="text" id="commune" name="commune" placeholder="Nom de la commune..."class="form-control" required>
+                <input type="text" id="commune" name="commune" placeholder="Nom de la commune..." class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="accessibilite">Accessibilité : <span class="required">*</span></label>
-                <textarea class="col-12"id="accessibilite" name="accessibilite" placeholder="Adapté aux personnes à mobilité réduite..." rows="3"class="form-control" required></textarea>
+                <textarea class="col-12" id="accessibilite" name="accessibilite" placeholder="Adapté aux personnes à mobilité réduite..." rows="3" class="form-control" required></textarea>
             </div>
             <div class="form-group">
                 <label for="ouverture">Ouverture : <span class="required">*</span></label>
-                <textarea class="col-12"id="ouverture" name="ouverture" placeholder="Libre d'accès ou infos sur la fermeture..." rows="3"class="form-control" required></textarea>
+                <textarea class="col-12" id="ouverture" name="ouverture" placeholder="Libre d'accès ou infos sur la fermeture..." rows="3" class="form-control" required></textarea>
             </div>
-
-            <button type="submit" name="submit" class="btn btn-primary btn-adm-mov">Ajouter la Contrée</button>
+            <button id="addButton" type="submit" class="btn">Ajouter la contrée</button>
         </form>
     </section>
     <section id="deleteContree" class="container bg-contact text-right mt-4">
-    <h2>Supprimer une contrée</h2>
-    <hr>
-    <form id="deleteForm" action="?action=deleteContree" method="post">
-        <div class="form-group mt-4">
-            <label for="idContree">Titre de la contrée<span class="required">*</span></label>
-            <select id="idContree" name="idContree" class="form-control" required>
-                <option value="">--- Sélectionner une contrée ---</option>
-                <?php
+        <h2>Supprimer une contrée</h2>
+        <hr>
+        <form id="deleteForm" action="?action=deleteContree" method="post">
+            <div class="form-group mt-4">
+                <label for="idContree">Titre de la contrée<span class="required">*</span></label>
+                <select id="idContree" name="idContree" class="form-control" required>
+                    <option value="">--- Sélectionner une contrée ---</option>
+                    <?php
 
-                // Vérifier si $contrees contient des données avant de l'utiliser
-                if ($contrees) {
-                    foreach ($contrees as $contree) {
-                        $id = $contree['Id_contree']; // ID de la contrée
-                        $titre = $contree['titre']; // Titre de la contrée
-                        echo "<option value=\"$id\">$titre</option>";
+                    // Vérifier si $contrees contient des données avant de l'utiliser
+                    if ($contrees) {
+                        foreach ($contrees as $contree) {
+                            $id = $contree['Id_contree']; // ID de la contrée
+                            $titre = $contree['titre']; // Titre de la contrée
+                            echo "<option value=\"$id\">$titre</option>";
+                        }
                     }
-                }
-                ?>
-            </select>
-        </div>
-        <button id="deleteButton" type="button" class="btn btn-primary">Supprimer la contrée</button>
-    </form>
-</section>
-        <!-- Le script qui permet de confirmer la suppression de la contrée  -->
-        <script src="Public/js/deleteContree.js"></script>
+                    ?>
+                </select>
+            </div>
+            <button id="deleteButton" type="submit" class="btn">Supprimer la contrée</button>
+        </form>
+    </section>
+    <!-- Le script qui permet de confirmer la suppression de la contrée  -->
+    <script src="Public/js/deleteContree.js"></script>
 </div>
 <?php
 // Inclusion du fichier de vue pour le pied de page
