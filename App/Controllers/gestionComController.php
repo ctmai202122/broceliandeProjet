1cdc1a019b1742a44ee0ad9863c3852fb6dd9d9a
ctmai202122 <ctmai2021@gmail.com>
2023-05-29 23:19:42 +0200

    Correction Message Co pour Moderation

--- App/Controllers/gestionComController.php
+++ App/Controllers/gestionComController.php
@@ -6,12 +6,10 @@ use Broceliande\Models\Commentaire;
 if (isset($_SESSION['admin'])) {
     // Récupération des commentaires non modérés (statut=0) avec leurs titres de contrée
     $commentaires = Commentaire::getAllExtended(0);
-
     include_once(__DIR__ . '/../Views/viewGestionCommentaires.php');
 } else {
     // Redirection vers la page de connexion
     $erreur = "Vous devez être connecté pour accéder à la page d'administration.";
-
     include_once(__DIR__ . '/../Views/viewConnexion.php');
 }
 ?>
