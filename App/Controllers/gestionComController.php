d6347325528e4111ff60ce72b726c9809d87c06e
ctmai202122 <ctmai2021@gmail.com>
2023-05-29 10:42:01 +0200

    corections pour commentaires

--- App/Controllers/gestionComController.php
+++ App/Controllers/gestionComController.php
@@ -3,10 +3,16 @@ namespace Broceliande\Controllers;
 
 use Broceliande\Models\Commentaire;
 
-// Récupération des commentaires non modérés (statut=0) avec leurs titres de contrée
-$commentaires = Commentaire::getAllExtended(0);
+if (isset($_SESSION['admin'])) {
+    // Récupération des commentaires non modérés (statut=0) avec leurs titres de contrée
+    $commentaires = Commentaire::getAllExtended(0);
 
-include_once(__DIR__ . '/../Views/viewGestionCommentaires.php');
+    include_once(__DIR__ . '/../Views/viewGestionCommentaires.php');
+} else {
+    // Redirection vers la page de connexion
+    // !!! TO DO - prévoir un message du genre "Vous devez être connecté pour accéder au panneau d'administration" 
+    include_once(__DIR__ . '/../Views/viewConnexion.php');
+}
 
 /* ---
 // Vérification que le formulaire a été soumis
