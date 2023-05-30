2ced31103fb81ac8e1ed68e2b5cb70955aea5567
ctmai202122 <ctmai2021@gmail.com>
2023-05-30 11:59:15 +0200

    correction message moderation commentaires

--- App/Controllers/moderateComController.php
+++ App/Controllers/moderateComController.php
@@ -17,33 +17,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             Commentaire::delete($idCommentaire);
         }
     }
-
     // Stockage d'un message de confirmation dans une variable de session
     $_SESSION['message'] = "Les commentaires ont été modérés avec succès.";
-    //$message = "Les commentaires ont été modérés avec succès.";
+
     // Redirection vers la page de moderation des commentaires
     header('Location: ?action=gestionCommentaire');
-    //include_once (__DIR__ . '/../Views/viewGestionCommentaires.php');
-    // unset ($_SESSION['message']);
-    //$message ="";
 }
-
-/* ---
-// Vérification si l'identifiant de la contrée est présent dans l'URL
-if (isset($_GET['id'])) {
-    $id_contree = $_GET['id']; // Récupération de l'identifiant de la contrée depuis l'URL
-
-    // Utilisez l'identifiant de la contrée pour récupérer les commentaires correspondants depuis la base de données
-    $commentaires = Commentaire::getByIdContree($id_contree);
-
-    // Récupérer le titre de la contrée
-    $titre_contree = Commentaire:: getAllExtended();
-
-    // Inclure la vue pour afficher les commentaires et le formulaire de modération
-    include('App/Views/viewCommentaires.php');
-} else {
-    // L'identifiant de la contrée n'est pas présent dans l'URL
-    $erreur = "L'identifiant de la contrée est manquant.";
-}
---- */
 ?>
