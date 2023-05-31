b4e3a487ce124154da45ac11f3ad2bb7a96bc7ed
ctmai202122 <ctmai2021@gmail.com>
2023-05-31 11:48:48 +0200

    Ajout tri commentaires asc et desc

--- Public/js/triCommentaire.js
+++ Public/js/triCommentaire.js
@@ -21,26 +21,48 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    let currentSort = {
+        column: -1,
+        direction: 'asc'
+    };
+
     function sortTable(column) {
         const table = document.querySelector('.tableModeration');
         const tbody = table.querySelector('tbody');
         const rows = Array.from(tbody.getElementsByTagName('tr'));
 
-        const currentSort = {
-            column: column,
-            direction: 'asc'
-        };
+        if (currentSort.column === column) {
+            currentSort.direction = currentSort.direction === 'asc' ? 'desc' : 'asc';
+        } else {
+            currentSort.column = column;
+            currentSort.direction = 'asc';
+        }
 
         rows.sort((a, b) => {
             const aCellValue = a.cells[column].textContent.trim().toLowerCase();
             const bCellValue = b.cells[column].textContent.trim().toLowerCase();
 
-            const comparison = aCellValue.localeCompare(bCellValue);
+            let comparison = 0;
+
+            if (aCellValue > bCellValue) {
+                comparison = 1;
+            } else if (aCellValue < bCellValue) {
+                comparison = -1;
+            }
+
             return currentSort.direction === 'asc' ? comparison : -comparison;
         });
 
         rows.forEach(row => tbody.appendChild(row));
 
-        currentSort.direction = currentSort.direction === 'asc' ? 'desc' : 'asc';
+        // Supprimer les icônes de tri de toutes les colonnes
+        const sortIcons = document.querySelectorAll('.sort-icon');
+        sortIcons.forEach(icon => {
+            icon.classList.remove('fa-sort-up', 'fa-sort-down');
+        });
+
+        // Ajouter l'icône de tri à la colonne en cours
+        const currentSortIcon = table.querySelector(`#col-${column} .sort-icon`);
+        currentSortIcon.classList.add(currentSort.direction === 'asc' ? 'fa-sort-up' : 'fa-sort-down');
     }
 });
