6ddca3026cc136492f1ffe92107428d80e71f9aa
ctmai202122 <ctmai2021@gmail.com>
2023-05-31 14:46:44 +0200

    Titre menu admin

--- App/Views/viewMenuAdmin.php
+++ App/Views/viewMenuAdmin.php
@@ -1,7 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center">
+<nav class="navbar navbar-expand-lg navbar-dark justify-content-center">
     <div class="navAdmin">
         <!-- Le bouton burger admin qui permettra de déclencher le menu déroulant -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuAdmin" aria-controls="menuAdmin" aria-expanded="false" aria-label="Toggle navigation">
+            <h4> Menu d'administration </h4>
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="menuAdmin">
