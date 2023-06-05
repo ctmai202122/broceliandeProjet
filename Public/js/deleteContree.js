f1532a84b405a31efae8fac6219815d52e937149
ctmai202122 <ctmai2021@gmail.com>
2023-06-05 15:22:38 +0200

    modifications jQuery

--- Public/js/deleteContree.js
+++ Public/js/deleteContree.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Récupération de l'élément du formulaire
     const deleteForm = $('#deleteForm');
-  
+
     // Ajout d'un gestionnaire d'événement 'submit' sur le formulaire de suppression
-    deleteForm.submit(function(event) {
-      // Popup de confirmation (retourne TRUE si confirmé, FALSE si annulé)
-      const confirmation = confirm('Êtes-vous sûr de vouloir supprimer cette contrée ?');
-  
-      // Si non confirmé -> annulation de l'événement
-      if (!confirmation) {
-        event.preventDefault();
-      }
+    deleteForm.submit(function (event) {
+        // Popup de confirmation (retourne TRUE si confirmé, FALSE si annulé)
+        const confirmation = confirm('Êtes-vous sûr de vouloir supprimer cette contrée ?');
+
+        // Si non confirmé -> annulation de l'événement
+        if (!confirmation) {
+            event.preventDefault();
+        }
     });
-  });
-  
+});
+
