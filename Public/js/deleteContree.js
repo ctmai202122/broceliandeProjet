abd763c6a93104326bfe03b52f87e8417db01402
ctmai202122 <ctmai2021@gmail.com>
2023-06-06 06:11:13 +0200

    redim images et chargement sécurisé des scripts

--- Public/js/deleteContree.js
+++ Public/js/deleteContree.js
@@ -1,14 +1,19 @@
-// Récupération de l'élément du formulaire
-const deleteForm = document.querySelector('#deleteForm');
 
-// Ajout d'un écouteur d'événement 'submit' sur le formulaire de suppression
-deleteForm.addEventListener('submit', function (event) {
-    // Popup de confirmation (retourne TRUE si confirmé, FALSE si annulé)
-    const confirmation = confirm('Êtes-vous sûr de vouloir supprimer cette contrée ?');
+// Lorsque le contenu de la page est entièrement chargé..
+document.addEventListener('DOMContentLoaded', function () {
+    // Récupération de l'élément du formulaire
+    const deleteForm = document.querySelector('#deleteForm');
 
-    // Si non confirmé -> annulation de l'événement
-    if (!confirmation) {
-        event.preventDefault();
-    }
+    // Ajout d'un écouteur d'événement 'submit' sur le formulaire de suppression
+    deleteForm.addEventListener('submit', function (event) {
+        // Popup de confirmation (retourne TRUE si confirmé, FALSE si annulé)
+        const confirmation = confirm('Êtes-vous sûr de vouloir supprimer cette contrée ?');
+
+        // Si non confirmé -> annulation de l'événement
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
 });
+
 
--- Public/js/deleteLegende.js
+++ Public/js/deleteLegende.js
@@ -1,13 +1,16 @@
-// Récupération de l'élément du formulaire
-const deleteForm = document.querySelector('#deleteForm');
+// Lorsque le contenu de la page est entièrement chargé...
+document.addEventListener('DOMContentLoaded', function () {
+    // Récupération de l'élément du formulaire
+    const deleteForm = document.querySelector('#deleteForm');
 
-// Ajout d'un écouteur d'événement 'submit' sur le formulaire de suppression
-deleteForm.addEventListener('submit', function (event) {
-    // Popup de confirmation (retourne TRUE si confirmé, FALSE si annulé)
-    const confirmation = confirm('Êtes-vous sûr de vouloir supprimer cette légende ?');
+    // Ajout d'un écouteur d'événement 'submit' sur le formulaire de suppression
+    deleteForm.addEventListener('submit', function (event) {
+        // Popup de confirmation (retourne TRUE si confirmé, FALSE si annulé)
+        const confirmation = confirm('Êtes-vous sûr de vouloir supprimer cette légende ?');
 
-    // Si non confirmé -> annulation de l'événement
-    if (!confirmation) {
-        event.preventDefault();
-    }
+        // Si non confirmé -> annulation de l'événement
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
 });
